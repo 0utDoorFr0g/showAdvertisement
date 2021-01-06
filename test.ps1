4f0860bd0a6118c9612767b4af13a3a577ad3e5f
0utDoorFr0g <63755945+0utDoorFr0g@users.noreply.github.com>
2021-01-06 17:53:28 +0900

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,5 @@
-[System.Windows.MessageBox]::Show('test')
+$msgBody = "blabla is blabla"
+$msgTitle = "Question!"
+$msgButton = 'YesNoCancel'
+$msgImage = 'Question'
+$Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
